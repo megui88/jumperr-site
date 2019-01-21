6c298b1da3a58bfe52cc7864ef6394c85fb932c8
Pedro Elias <pedro.gonzalez@jumperr.com>
2019-01-21 15:31:38 -0400

    carreras

---
+++ resources/js/components/carreras/sectionContactoCarreras.vue
@@ -0,0 +1,35 @@
+<template>
+    <div id="section_contacto_carrera">
+        <section class="contacto">
+            <div class="container">
+                <div class="row">
+                    <div class="col-12 col-md-8 mx-auto">
+                        <h3 class="text-center text-white">¿Listo para trabajar con nosotros?</h3>
+                        <p class="lead text-center text-white">
+                            Impúlsate con Jumperr
+                        </p>
+                        <div class="text-center">
+                            <a href="#" class="btn btn-primary text-uppercase">Contáctanos</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+    </div>
+</template>
+<style scoped>
+.contacto{
+    background-color: #fff;
+    background-image: url('/images/carreras/banner-2.png');
+    background-size: cover;
+    background-position: top center;
+    background-repeat: no-repeat;
+
+    width: 100%;
+    min-height: 700px;
+
+    margin-bottom: -100px;
+    display: grid;
+    align-items: center;
+}
+</style>
---
+++ resources/js/components/carreras/sectionProcesoCarreras.vue
@@ -0,0 +1,92 @@
+<template>
+    <div id="section_proceso_carreras">
+        <section class="wrapper">
+            <article class="mb-4">
+                <h3 class="title-border-bottom text-center text-white">Proceso de reclutamiento y selección</h3>
+            </article>
+            <aside>
+                <div class="item">
+                    <img src="/images/carreras/telefono-min.png" alt="Jumperr proceso de carreras">
+                    <strong class="d-block">1. Entrevista por teléfono</strong>
+                </div>
+                <div class="item">
+                    <img src="/images/carreras/user-min.png" alt="Jumperr proceso de carreras">
+                    <strong class="d-block">2. Primera Entrevista</strong>
+                </div>
+                <div class="item">
+                    <img src="/images/carreras/tuerca-min.png" alt="Jumperr proceso de carreras">
+                    <strong class="d-block">3. Prueba Técnica</strong>
+                </div>
+                <div class="item">
+                    <img src="/images/carreras/moneda-min.png" alt="Jumperr proceso de carreras">
+                    <strong class="d-block">4. Oferta Salarial</strong>
+                </div>
+                <div class="item">
+                    <img src="/images/carreras/logocircular copy.svg" alt="Jumperr proceso de carreras">
+                    <strong class="d-block">5. Bienvenido al Team Jumperr</strong>
+                </div>
+            </aside>
+        </section>
+    </div>
+</template>
+<style scoped>
+    #section_proceso_carreras{
+        background-image: url('/images/carreras/imagen_2-min.jpg');
+        background-size: cover;
+        background-repeat: no-repeat;
+        background-position: center;
+        width: 100%;
+        min-height: 600px;
+        padding: 50px 0;
+        display: grid;
+        align-items: center;
+    }
+    .title-border-bottom:after {
+        content: ' ';
+        border-bottom: 1px solid #f26336;
+        display: block;
+        width: 160px;
+        margin: 1.3rem auto 1.1rem;
+    }
+    .wrapper{
+        width: 100%;
+        max-width: 1140px;
+        height: auto;
+        min-height: 100%;
+        margin: auto;
+        padding-right: 15px;
+        padding-left: 15px;
+        display: grid;
+        align-content: center;
+    }
+    aside{
+        display: flex;
+        flex-direction: column;
+        justify-content: space-between;
+        align-items: flex-start;
+    }
+    .item{
+        color: #fff;
+        text-align: center;
+        margin-bottom: 2rem;
+        width: 100%;
+    }
+    .item img{
+        height: 100px;
+        margin-bottom: 1rem;
+    }
+    @media (min-width: 768px) {
+        aside{
+            display: flex;
+            flex-direction: row;
+            justify-content: space-between;
+            align-items: flex-start;
+        }
+        .item{
+            color: #fff;
+            text-align: center;
+            margin-bottom: 2rem;
+            width: 100%;
+        }
+    }
+</style>
