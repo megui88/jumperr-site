6a1e30f4c1e796338594474e8634bd56b3f7fbe6
Pedro Elias <pedro.gonzalez@jumperr.com>
2019-01-21 05:06:11 -0400

    partners animaciones

--- resources/js/components/partners/sectionPartnersPresentation.vue
+++ resources/js/components/partners/sectionPartnersPresentation.vue
@@ -1,8 +1,10 @@
 <template>
     <div id="presentacion-partners">
         <section class="wrapper">
+            <!-- fondo -->
             <img class="img-bg-1"  src="/images/partners/fondo_foto.png" alt="Jumperr background">
             <img class="img-bg-2" src="/images/partners/fonod_foto2.png" alt="Jumperr background">
+            <!-- fin del fondo -->
             <div class="container">
                 <div class="row">
                     <div class="col-12 col-md-8 mx-auto">
@@ -16,6 +18,17 @@
                     </div>
                 </div>
             </div>
+
+            <!-- imagenes flotando | animaciones -->
+            <!-- izquierda -->
+            <img class="img-float-1 d-none d-md-inline-block" src="/images/iconos/nube_2.svg" alt="elemento jumperr">
+            <img class="img-float-2" src="/images/iconos/signodemas.svg" alt="elemento jumperr">
+            <img class="img-float-3  d-none d-md-inline-block" src="/images/iconos/circulowhite.svg" alt="elemento jumperr">
+            <!-- derecha -->
+            <img class="img-float-4  d-none d-md-inline-block" src="/images/iconos/circulogrueso.svg" alt="elemento jumperr">
+            <img class="img-float-5" src="/images/iconos/signodemas.svg" alt="elemento jumperr">
+            <div class="circle-pulse"></div>
+            <img class="img-float-6" src="/images/iconos/circulo-white.svg" alt="elemento jumperr">
         </section>
     </div>
 </template>
@@ -24,7 +37,6 @@
         width: 100%;
         min-height: 600px;
         position: relative;
-        z-index: -1;
         background-color: #fff;
         display: grid;
         align-items: center;
@@ -85,4 +97,223 @@
             object-fit: cover;
         }
     }
+
+    /* animaciones */
+    .circle-pulse {
+        position: absolute;
+        bottom: 280px;
+        right: 30px;
+        display: block;
+        width: 22px;
+        height: 22px;
+        border-radius: 50%;
+        background: #fff;
+        cursor: pointer;
+        box-shadow: 0 0 0 rgba(255,255,255, 0.4);
+        animation: pulse 2s infinite;
+    }
+    @keyframes pulse {
+        0% {
+            -moz-box-shadow: 0 0 0 0 rgba(255,255,255, 0.4);
+            box-shadow: 0 0 0 0 rgba(255,255,255, 0.4);
+        }
+        70% {
+            -moz-box-shadow: 0 0 0 10px rgba(255,255,255, 0);
+            box-shadow: 0 0 0 10px rgba(255,255,255, 0);
+        }
+        100% {
+            -moz-box-shadow: 0 0 0 0 rgba(255,255,255, 0);
+            box-shadow: 0 0 0 0 rgba(255,255,255, 0);
+        }
+    }
+
+    .img-float-1{
+        position: absolute;
+        top: 100px;
+        left: -45px;
+        display: inline-block;
+        width: 200px;
+        animation: float 20s linear infinite;
+    }
+    .img-float-2{
+        position: absolute;
+        bottom: 40px;
+        left: 35px;
+        display: inline-block;
+        width: 22px;
+        animation: saltar 2s linear infinite;
+    }
+    .img-float-3{
+        position: absolute;
+        bottom: 130px;
+        left: -35px;
+        display: inline-block;
+        width: 100px;
+        height: 100px;
+        animation: opacidad 5s linear infinite;
+    }
+    .img-float-4{
+        position: absolute;
+        top: 120px;
+        right: -45px;
+        display: inline-block;
+        width: 100px;
+        height: 100px;
+        animation: opacidad 5s linear infinite;
+    }
+    .img-float-5{
+        position: absolute;
+        top: 240px;
+        right: 25px;
+        display: inline-block;
+        width: 22px;
+        height: 22px;
+        animation: saltar 2s linear infinite;
+    }
+    .img-float-6{
+        position: absolute;
+        bottom: 175px;
+        right: -25px;
+        display: inline-block;
+        width: 70px;
+        height: 70px;
+        animation: opacidad 5s linear infinite;
+    }
+
+@keyframes opacidad {
+    0%{
+        transform: rotate(0deg);
+        opacity: 1;
+    }
+    50%{
+        opacity: .5;
+        transform: rotate(180deg);
+    }
+    100%{
+        transform: rotate(360deg);
+        opacity: 1;
+
+    }
+}
+
+@keyframes saltar {
+    0% {
+		transform: translatey(0px);
+	}
+	50% {
+		transform: translatey(-20px);
+	}
+	100% {
+		transform: translatey(0px);
+	}
+}
+
+@keyframes float {
+	0% {
+        left: -5%;
+        /* transform: translateY(-10px); */
+        opacity: 1;
+	}
+    5% {
+        left: 5%;
+        /* transform: translateY(10px); */
+        opacity: 1;
+	}
+    10% {
+        left: 10%;
+        /* transform: translateY(-10px); */
+        opacity: 1;
+	}
+    15% {
+        left: 15%;
+        /* transform: translateY(10px); */
+        opacity: 1;
+	}
+
+    20% {
+        left: 20%;
+        /* transform: translateY(-10px); */
+        opacity: 1;
+	}
+    25% {
+        left: 25%;
+        /* transform: translateY(10px); */
+        opacity: 1;
+	}
+    30% {
+        left: 30%;
+        /* transform: translateY(-10px); */
+        opacity: 1;
+	}
+    35% {
+        left: 35%;
+        /* transform: translateY(10px); */
+        opacity: 1;
+	}
+    40% {
+        left: 40%;
+        /* transform: translateY(-10px); */
+        opacity: 1;
+	}
+    45% {
+        left: 45%;
+        /* transform: translateY(10px); */
+        opacity: 1;
+	}
+	50% {
+        left: 50%;
+        /* transform: translateY(-10px); */
+        opacity: 1;
+	}
+    55% {
+        left: 55%;
+        /* transform: translateY(10px); */
+        opacity: 1;
+	}
+    60% {
+        left: 60%;
+        /* transform: translateY(-10px); */
+        opacity: 1;
+	}
+    65% {
+        left: 65%;
+        /* transform: translateY(10px); */
+        opacity: 1;
+	}
+    70% {
+        left: 70%;
+        /* transform: translateY(-10px); */
+        opacity: 1;
+	}
+    75% {
+        left: 75%;
+        /* transform: translateY(10px); */
+        opacity: 1;
+	}
+    80% {
+        left: 80%;
+        /* transform: translateY(-10px); */
+        opacity: 1;
+    }
+    85% {
+        left: 85%;
+        /* transform: translateY(10px); */
+        opacity: 1;
+    }
+    90% {
+        left: 90%;
+        /* transform: translateY(-10px); */
+        opacity: 1;
+	}
+    95% {
+        left: 100%;
+        /* transform: translateY(10px); */
+        opacity: 1;
+	}
+	100% {
+        left: 150%;
+        /* transform: translateY(-10px); */
+        opacity: 0;
+	}
+}
 </style>
---
+++ resources/js/components/partners/sectionPartnersSevices.vue
@@ -0,0 +1,198 @@
+<template>
+    <div class="bg-white">
+        <div id="servicesPartners">
+            <div class="container">
+                <div class="row">
+                    <div class="col-12">
+                        <h3 class="text-center title-border-bottom">Proveemos servicios digitales  de calidad</h3>
+                    </div>
+                    <div class="col-12">
+                        <swiper :options="swiperOption" class="p-md-4">
+                            <swiper-slide v-for="(service, index) in services" :key="index">
+                                <div class="cart">
+                                    <div class="cart-header">
+                                        <div class="image">
+                                            <img :src="service.icono" :alt="service.title">
+                                        </div>
+                                    </div>
+                                    <div class="cart-body">
+                                        <div class="title">
+                                            <h3 class="">{{service.title}}</h3>
+                                        </div>
+                                        <p v-html="service.description"></p>
+                                    </div>
+                                </div>
+                            </swiper-slide>
+                                <div class="swiper-pagination swiper-pagination-x" slot="pagination"></div>
+                        </swiper>
+                    </div>
+                </div>
+            </div>
+            <img class="img-float-1 d-none d-md-inline-block" src="/images/iconos/nube_2.svg" alt="elemento jumperr">
+        </div>
+    </div>
+</template>
+<script>
+export default {
+    data(){
+        return {
+            services: [
+                {
+                    icono:  '/images/partners/001-sitemap.svg',
+                    title: 'DOMINIO & HOSTING',  
+                    description: `Identificación del dominio de un nuevo registro o transferencia del dominio existente, con su correspondiente web mail. El servicio de alojamiento se proporcionará a través de nuestros servidores.`
+                },
+                {
+                    icono:  '/images/partners/002-paper.svg',
+                    title: 'BOLETÍN',  
+                    description: `Gestión de una lista de contactos personales con la posibilidad de enviar correo electrónico y boletines personalizados con software profesional.`
+                },
+                {
+                    icono:  '/images/partners/009-web-site.svg',
+                    title: 'ENTREVISTA',  
+                    description: `Realización de una entrevista telefónica para recopilar información específica sobre tu empresa`
+                },  
+                {
+                    icono:  '/images/partners/003-picture.svg',
+                    title: 'BANNERS E IMÁGENES',  
+                    description: `Creación y publicación de banners e imágenes (usando fotos del cliente o categoría de los archivos fotográficos) cargadas por el equipo editorial.`
+                },  
+                {
+                    icono:  '/images/partners/004-global.svg',
+                    title: 'RESPONSIVE',  
+                    description: `Optimización de sitios web para computadoras, teléfonos inteligentes y tablets.`
+                },
+                {
+                    icono:  '/images/partners/005-close-envelope.svg',
+                    title: 'NEWSLETTER',  
+                    description: `Gestion de una lista de contactos personales con la posibilidad de enviar correo electronico y newsletters personalizados por medio de un software profesional.`
+                },  
+                {
+                    icono:  '/images/partners/006-graph.svg',
+                    title: 'PANEL DE CONTROL',  
+                    description: `Acceso a un panel de control para la gestión autónoma de sus textos, imágenes, videos y ofertas.`
+                },  
+                {
+                    icono:  '/images/partners/007-pencil-edit-button.svg',
+                    title: 'REDACCIÓN DE CONTENIDO',  
+                    description: `Creación e inserción de contenido textual en una perspectiva SEO para una correcta optimización de su sitio web.`
+                },           
+                {
+                    icono:  '/images/partners/008-content.svg',
+                    title: 'BLOG',  
+                    description: `Apertura y configuración, primer post de bienvenida, guía y manual de usuario.`
+                },  
+                {
+                    icono:  '/images/partners/009-web-site.svg',
+                    title: 'ASISTENCIA WEB',  
+                    description: `Soporte y asistencia de un equipo web profesional, hasta que su sitio esté en línea y se vcumplan 7 tickets de asistencia post-publicación.`
+                },
+                {
+                    icono:  '/images/partners/010-trello-website-logo.svg',
+                    title: 'SECCIONES DEL SITIO',  
+                    description: `
+                    <ol>
+                        <li>Página de inicio</li>
+                        <li>Nosotros</li>
+                        <li>Galería y Banners</li>
+                        <li>Ofertas</li>
+                        <li>Blog</li>
+                        <li>Contactos</li>
+                        <li>Comercio Electrónico / Reserva, etc</li>
+                    </ol>
+                    `
+                },  
+                {
+                    icono:  '/images/partners/011-label.svg',
+                    title: 'TAG',  
+                    description: `
+                    Sistema de asignación de etiquetas TAG:<br>
+                    Palabra clave + Municipio<br>
+                    Palabra clave + Provincia<br>
+                    Palabra clave + Pipo<br>
+                    Palabra clave + Social Ash #<br>
+                    `
+                },  
+                {
+                    icono:  '/images/partners/012-cart.svg',
+                    title: 'E-COMMERCE',  
+                    description: `Acceso al panel para la publicación y venta de productos o servicios.`
+                },
+                {
+                    icono:  '/images/partners/013-smartphone.svg',
+                    title: 'SOCIAL MEDIA',  
+                    description: `Apertura de la FanPage de Facebook, Google+, Google MyBussiness e Instagram Plug-in automático para compartir sus noticias / promo de su sitio web.`
+                },  
+
+
+            ],
+            swiperOption: {
+            slidesPerView: 3,
+            slidesPerColumn: 5,
+            slidesPerColumnFill: 'row',
+            spaceBetween: 40,
+            autoplay: {
+                delay: 3000,
+            },
+            pagination: {
+                el: '.swiper-pagination',
+                clickable: true,
+                renderBullet(index, className) {
+                    return `<span class="${className} swiper-pagination-bullet-x"></span>`
+                }
+            },
+            breakpoints: {
+                380: {
+                slidesPerView: 1,
+                spaceBetween: 30,
+                slidesPerColumn: 1,
+
+                },
+                768: {
+                slidesPerView: 1,
+                spaceBetween: 30,
+                slidesPerColumn: 1,
+
+                },
+                992: {
+                slidesPerView: 3,
+                spaceBetween: 40,
+                slidesPerColumn: 2,
+                }
+            }
+        }
+        }
+    }
+}
+</script>
+
+<style scoped>
+    #servicesPartners{
+        -webkit-clip-path: polygon(0 4%, 100% 0, 100% 100%, 0% 100%);
+        clip-path: polygon(0 4%, 100% 0, 100% 100%, 0% 100%);
+        position: relative;
+        background-color: #f8f8f8;
+        padding-top: 80px;
+    }
+    .title-border-bottom:after {
+        content: ' ';
+        border-bottom: 1px solid #f26336;
+        display: block;
+        width: 160px;
+        margin: 1.3rem auto 1.1rem;
+    }
+    .cart-header .image img{
+        width: 46px;
+    }
+    .title h3{
+        font-size: 1.4rem;
+        margin-top: 1rem;
+    }
+    .cart-body p{
+        font-size: 1.1rem;
+    }
+
+    /* animaciones */
+    
+</style>
+
