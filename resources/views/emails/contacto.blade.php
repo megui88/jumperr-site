4b4fb733dbd423623064c98c46c075d2314ba797
elopez <euglis.lopez@jumperr.com>
2019-01-27 02:53:37 -0400

    add name jumperr on email contact and newsletter, requerid logo jumperr

--- resources/views/emails/contacto.blade.php
+++ resources/views/emails/contacto.blade.php
@@ -293,7 +293,7 @@
                                                             <tbody>
                                                                 <tr style="padding:0;text-align:left;vertical-align:top">
                                                                     <th style="margin:auto;color:#0a0a0a;font-family:Helvetica,Arial,sans-serif;font-size:16px;font-weight:400;line-height:1.3;margin:0;padding:0;text-align:center"><img src="{{ asset('/images/logos/solaria-logo.png') }}" style="-ms-interpolation-mode:bicubic;clear:both;display:block;max-width:500%;outline:0;text-decoration:none;width: 80px; margin: auto;"></th>
-     
+
                                                                 </tr>
                                                             </tbody>
                                                         </table>
@@ -332,7 +332,7 @@
                                                                         </table>
 
 
-                                                                        <p class="text-left" style="Margin:0;Margin-bottom:10px;color:#0a0a0a;font-family:Helvetica,Arial,sans-serif;font-size:16px;font-weight:400;line-height:1.3;margin:0;margin-bottom:10px;padding:0;text-align:left">Hola, <strong>Solaria Vacanza</strong></p>
+                                                                        <p class="text-left" style="Margin:0;Margin-bottom:10px;color:#0a0a0a;font-family:Helvetica,Arial,sans-serif;font-size:16px;font-weight:400;line-height:1.3;margin:0;margin-bottom:10px;padding:0;text-align:left">Hola, <strong>Jumperr</strong></p>
 
 
                                                                         @if(!empty($name))
@@ -377,7 +377,7 @@
                                             <tbody style="background-color: #000000;">
                                                 <tr style="padding:0;text-align:left;vertical-align:top">
                                                     <th class="small-12 large-3 columns first" style="Margin:0 auto;color:#0a0a0a;font-family:Helvetica,Arial,sans-serif;font-size:16px;font-weight:400;line-height:1.3;margin:0 auto;padding:0;padding-left:16px;padding-right:8px;padding-top:16px;text-align:left;width:129px">
-                                                        <p class="text-center" style="Margin:0;Margin-bottom:10px;color:#fff;font-family:Helvetica,Arial,sans-serif;font-size:12px;font-weight:400;line-height:1.3;margin:0;margin-bottom:10px;padding:0;text-align:center"><b>© Solaria Vacanze 2018 | <a href="{{url('/')}}/policies" style="color:#fff;"> Informativa sulla Privacy</a> | <a href="{{url('/')}}/terms" style="color:#fff;">Termini e Condizioni</a> </b></p>
+                                                        <p class="text-center" style="Margin:0;Margin-bottom:10px;color:#fff;font-family:Helvetica,Arial,sans-serif;font-size:12px;font-weight:400;line-height:1.3;margin:0;margin-bottom:10px;padding:0;text-align:center"><b>© Jumperr 2019 | <a href="{{url('/')}}/policies" style="color:#fff;"> Informativa sulla Privacy</a> | <a href="{{url('/')}}/terms" style="color:#fff;">Termini e Condizioni</a> </b></p>
                                                         <center data-parsed="" style="min-width:97px;width:100%"><a href="{{ url('/') }}"><img width="160px" height="43px" src="{{ asset('/images/logos/jumperr.png') }}" style="-ms-interpolation-mode:bicubic;clear:both;display:block;max-width:100%;outline:0;text-decoration:none;width:auto"></a></center>
                                                     </th>
                                                 </tr>
